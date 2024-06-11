2b0c1d34361e6773454b6b44b1f5f5370bf68824
Tewobsta Seyoum Assefa <155534908+Tewobsta-sa@users.noreply.github.com>
2024-06-12 01:27:03 +0300

    Create New button.java

---
+++ New button.java
@@ -0,0 +1,64 @@
+public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == saveMI) {
+            saveFile();
+        } else if (e.getSource() == openMI) {
+            openFile();
+        } else if (e.getSource() == newMI) {
+            newFile();
+        }
+    }
+
+    private void saveFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showSaveDialog(window);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            // Save the file
+            // ...
+        }
+    }
+
+    private void openFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(window);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            // Open the file
+            // ...
+        }
+    }
+
+    private void newFile() {
+        if (!textArea.getText().equals(currentText)) {
+            // Show a save confirmation dialog
+            int option = JOptionPane.showConfirmDialog(
+                    window,
+                    "The current text has been modified. Do you want to save it?",
+                    "Save File",
+                    JOptionPane.YES_NO_CANCEL_OPTION
+            );
+
+            // Handle the user's choice
+            switch (option) {
+                case JOptionPane.YES_OPTION:
+                    // Save the file
+                    saveFile();
+                    textArea.setText("");
+                    setTitle("Untitled");
+                    break;
+                case JOptionPane.NO_OPTION:
+                    // Don't save, clear the text area and set the title
+                    textArea.setText("");
+                    setTitle("Untitled");
+                    break;
+                case JOptionPane.CANCEL_OPTION:
+                    // Cancel the operation, do nothing
+                    break;
+            }
+        } else {
+            // No modifications, clear the text area and set the title
+            textArea.setText("");
+            setTitle("Untitled");
+        }
+    }
+}
